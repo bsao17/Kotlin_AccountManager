04620c9134d0df1435ddf886a378d65ce1e46e8f
bsao17 <declic62@gmail.com>
2021-12-22 20:33:21 +0100

    Add interface for currentAccount and ProfessionalAccount

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,49 @@
 import java.util.*
 
-class Account(val depot: Int){
+
+interface CurrentAccountActions {
+    fun retrieveMoney(value: Int)
+    fun addMoney(value: Int)
+    fun readAccount()
+}
+
+interface ProfessionalAccount {
+    fun retrieveMoney(value: Int)
+    fun addMoney(value: Int)
+    fun readAccount()
+    fun overdraftPossibilities(value: Int)
+}
+
+class CurrentAccount(depot: Int): CurrentAccountActions{
     var balance: Int = depot
 
-    fun retrieveMoney(value: Int){
-        if(balance > 0 && value < balance){
-            var solde: Int = this.balance-value
+    override fun retrieveMoney(value: Int) {
+        if (balance > 0 && value < balance) {
+            var solde: Int = this.balance - value
             println("A la date du : ${Date().toLocaleString()}, Vous avez retiré ${value} €, il vous reste ${solde}€ sur votre compte")
-        }else{
+        } else {
             println("A la date du : ${Date().toLocaleString()}, votre solde de compte est insuffisant !")
         }
     }
-    fun AddMoney(value: Int){
+
+    override fun addMoney(value: Int) {
         var newSolde: Int = this.balance + value
         println("A la date du : ${Date().toLocaleString()}, Après un dépot de $value€, il vous reste maintenant $newSolde€ sur votre compte")
     }
-}
- var BrunoAccount = Account(50)
-var EnzoAccount = Account(100)
 
-fun main(){
+    override fun readAccount() {
+        println("A la date du : ${Date().toLocaleString()}, le solde de votre compte est de $balance€ ")
+    }
+}
+
+
+var BrunoAccount = CurrentAccount(50)
+var EnzoAccount = CurrentAccount(100)
+
+fun main() {
     BrunoAccount.retrieveMoney(60)
     EnzoAccount.retrieveMoney(20)
-    BrunoAccount.AddMoney(50)
+    BrunoAccount.addMoney(50)
+    BrunoAccount.readAccount()
 
 }
